dcd9dbfe37e0faf19760b1a454baf297e369b8ee
Baptiste SIMON <beta@e-glop.net>
2015-11-10 11:02:47 +0100

    improving the ClassAnalyzer to be able to find deep Traits inside a wide tree of inheritances and traits inclusions

--- Tools/Reflection/ClassAnalyzer.php
+++ Tools/Reflection/ClassAnalyzer.php
@@ -10,9 +10,31 @@ class ClassAnalyzer extends \Knp\DoctrineBehaviors\Reflection\ClassAnalyzer
      * This static method returns back all traits used by a given class
      * recursively
      *
+     * @param $class            A ReflectionClass object or a string describing an existing class
      * @return array
      **/
-    public static function getTraits($class, array $traits = null)
+    public static function getTraits($class)
+    {
+        return self::_getTraits($class);
+    }
+    
+    /**
+     * hasTraits
+     *
+     * This static method returns back all traits used by a given class
+     * recursively
+     *
+     * @param $class            A ReflectionClass object or a string describing an existing class
+     * @param $trait            A string representing an existing trait
+     * @return boolean          TRUE or FALSE
+     **/
+    public function hasTrait(\ReflectionClass $class, $traitName, $isRecursive = true)
+    {
+        return in_array($traitName, self::getTraits($class));
+    }
+
+
+    private static function _getTraits($class, array $traits = null)
     {
         $rc = $class instanceof \ReflectionClass
             ? $class
@@ -24,7 +46,7 @@ class ClassAnalyzer extends \Knp\DoctrineBehaviors\Reflection\ClassAnalyzer
         foreach ( $rc->getTraits() as $trait )
         {
             $traits[] = $trait->name;
-            $traits = self::getTraits($trait, $traits);
+            $traits = self::_getTraits($trait, $traits);
         }
 
         return $traits;
