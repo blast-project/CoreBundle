0164f05abf7d8d15db84311407eac6569931a294
Baptiste SIMON <beta@e-glop.net>
2015-11-10 11:03:53 +0100

    adding ordering features for :   - BaseMappers   - GroupedMappers, their tabs / groups fieldsets

--- Admin/Admin.php
+++ Admin/Admin.php
@@ -126,6 +126,14 @@ abstract class Admin extends SonataAdmin
         }
 
         // if a grouped organization can be shapped
+        // options
+        $tabsOptions = null;
+        if ( isset($group['_options']) )
+        {
+            $tabsOptions = $group['_options'];
+            unset($group['_options']);
+        }
+        // content
         foreach ( $group as $tab => $tabcontent ) // loop on content...
         if ( self::arrayDepth($tabcontent) < 1 )
         {
@@ -135,6 +143,14 @@ abstract class Admin extends SonataAdmin
         }
         else
         {
+            // groups/withs order
+            $groupsOrder = null;
+            if ( isset($tabcontent['_options']) && isset($tabcontent['_options']['groupsOrder']) )
+            {
+                $groupsOrder = $tabcontent['_options']['groupsOrder'];
+                unset($tabcontent['_options']['groupsOrder']);
+            }
+            
             // tab
             $mapper->tab($tab, isset($tabcontent['_options']) ? $tabcontent['_options'] : array());
             if ( isset($tabcontent['_options']) )
@@ -168,11 +184,41 @@ abstract class Admin extends SonataAdmin
 
                 if ( $finalOrder != null )
                     $mapper->reorder($finalOrder);
-
+                
                 $mapper->end();
+            }
+            
+            // order groups / withs (using tabs, because they are prioritary at the end)
+            if ( isset($groupsOrder) )
+            {
+                $tabs = $mapper->getAdmin()->getFormTabs();
+                $groups = $tabs[$tab]['groups'];
+                $newgroups = array();
+                foreach ( $groupsOrder as $groupname )
+                if ( in_array("$tab.$groupname", $groups) )
+                    $newgroups[] = "$tab.$groupname";
+                foreach ( $groups as $groupname )
+                if ( !in_array($groupname, $newgroups) )
+                    $newgroups[] = $groupname;
+                $tabs[$tab]['groups'] = $newgroups;
+                $mapper->getAdmin()->setFormTabs($tabs);
             }
 
             $mapper->end();
+        }
+        
+        // order tabs
+        if ( isset($tabsOptions['tabsOrder']) )
+        {
+            $tabs = $mapper->getAdmin()->getFormTabs();
+            $newtabs = array();
+            foreach ( $tabsOptions['tabsOrder'] as $tabname )
+            if ( isset($tabs[$tabname]) )
+                $newtabs[$tabname] = $tabs[$tabname];
+            foreach ( $tabs as $tabname => $tab )
+            if ( !isset($newtabs[$tabname]) )
+                $newtabs[$tabname] = $tab;
+            $mapper->getAdmin()->setFormTabs($newtabs);
         }
 
         return $mapper;
