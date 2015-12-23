5117afde83c1df0b6b4017c55fda3d2e347f0e67
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2015-12-23 12:20:03 +0100

    librinfo.yml parser for ManyToManyManageer

--- Admin/Traits/ManyToManyManager.php
+++ Admin/Traits/ManyToManyManager.php
@@ -2,6 +2,8 @@
 
 namespace Librinfo\CoreBundle\Admin\Traits;
 
+use Librinfo\CoreBundle\Tools\Reflection\ClassAnalyzer;
+
 /**
  * This trait is used to delete many-to-many associated entities when the current
  * entity is not on the owning side
@@ -39,6 +41,8 @@ trait ManyToManyManager
      */
     protected function preUpdateManyToManyManager($object)
     {
+        $this->configureManyToManyManager();
+
         $rc = new \ReflectionClass($this->getClass());
         $remove_method = 'remove' . ucfirst($rc->getShortName());
 
@@ -52,5 +56,32 @@ trait ManyToManyManager
                     $this->getModelManager()->update($entity);
                 }
         }
+    }
+
+    private function configureManyToManyManager()
+    {
+        $librinfo = $this->getConfigurationPool()->getContainer()->getParameter('librinfo');
+        $key = 'managedManyToMany'; // name of the key in the librinfo.yml
+
+        // traits of the current Entity
+        $classes = ClassAnalyzer::getTraits($this->getClass());
+        // inheritance of the current Entity
+        foreach ( array_reverse(array($this->getClass()) + class_parents($this->getClass())) as $class )
+            $classes[] = $class;
+        // inheritance of the current Admin
+        foreach ( array_reverse(array($this->getOriginalClass()) + $this->getParentClasses()) as $admin )
+            $classes[] = $admin;
+
+        // merge configuration/parameters
+        foreach ( $classes as $class )
+        if ( isset($librinfo[$class])
+          && isset($librinfo[$class][$key]) )
+        {
+            if ( !is_array($librinfo[$class][$key]) )
+                $librinfo[$class][$key] = [$librinfo[$class][$key]];
+            $this->addManyToManyCollections($librinfo[$class][$key]);
+        }
+
+        return $this;
     }
 }
