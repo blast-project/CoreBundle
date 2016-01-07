28ede0eb34b61068870f97764de928db8525813e
Baptiste SIMON <beta@e-glop.net>
2016-01-07 11:13:48 +0100

    Adding some make'up in the code

--- DoctrineExtensions/LibrinfoWalker.php
+++ DoctrineExtensions/LibrinfoWalker.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Query\SqlWalker;
 
 class LibrinfoWalker extends SqlWalker
 {
-     /**
+    /**
      * Walks down a SelectClause AST node, thereby generating the appropriate SQL.
      *
      * @param $selectClause
@@ -16,14 +16,13 @@ class LibrinfoWalker extends SqlWalker
     {
         $sql = parent::walkSelectClause($selectClause);
 
-        if ($this->getQuery()->getHint('librinfoWalker.noIlike') === false) {
-                $sql = str_replace('LIKE', 'ILIKE', $sql);
-        }
+        if ($this->getQuery()->getHint('librinfoWalker.noIlike') === false)
+            $sql = str_replace(' LIKE ', ' ILIKE ', $sql);
 
         return $sql;
     }
 
-     /**
+    /**
      * Walks down a WhereClause AST node, thereby generating the appropriate SQL.
      *
      * @param $whereClause
@@ -33,9 +32,9 @@ class LibrinfoWalker extends SqlWalker
     {
         $sql = parent::walkWhereClause($whereClause);
 
-        if ($this->getQuery()->getHint('librinfoWalker.noIlike') === false) {
-                $sql = str_replace('LIKE', 'ILIKE', $sql);
-        }
+        if ($this->getQuery()->getHint('librinfoWalker.noIlike') === false)
+            $sql = str_replace(' LIKE ', ' ILIKE ', $sql);
+
         return $sql;
     }
 }
