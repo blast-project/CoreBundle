c03f7e520e961350f90d0b69fa535f10cbe17c93
Romain <romain.sanchez@libre-informatique.fr>
2016-11-28 14:03:55 +0100

    Updated licence header

--- Command/GenerateExtensionContainersCommand.php
+++ Command/GenerateExtensionContainersCommand.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * This file is part of the Blast package.
+ * This file is part of the Sonata Project package.
  *
- * (c) romain SANCHEZ <romain.sanchez@libre-informatique.fr>
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
