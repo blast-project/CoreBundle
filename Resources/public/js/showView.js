bfd322cb3e4189539ed3509506051182aed8b875
Romain <romain.sanchez@libre-informatique.fr>
2017-03-17 10:04:29 +0100

    Improved collections disaply in show view

--- Resources/public/js/showView.js
+++ Resources/public/js/showView.js
@@ -1,12 +1,15 @@
 $(document).ready(function () {
-    var collections = $('.blast-show-collection');
-    var container = collections.eq(0).closest('.box-primary').closest('div');
-    
-    collections.each(function () {
-        var new_container = $('<div>').insertAfter(container);
-        $('<div class="box box-primary">').appendTo(new_container).append($(this));
+    $('.blast-show-collection').each(function() {
+        var table = $(this)
+           .closest('table')
+           .not('blast-show-collection-element')
+        ;
+        
+        table
+            .closest('.box-primary')
+            .replaceWith($(this))
+        ;
     });
-    container.remove();
 });
 
 
