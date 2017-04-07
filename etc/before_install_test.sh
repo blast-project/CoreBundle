1917d597549ecbf8c1d07699d9a11abf4875ef4b
GlennCavarle <glenn.cavarle@libre-informatique.fr>
2017-04-07 11:32:47 +0200

    updated script permissions

--- etc/before_install_test.sh
+++ etc/before_install_test.sh
@@ -2,5 +2,7 @@
 set -ev
 
 # To be removed when following PR will be merged: https://github.com/travis-ci/travis-build/pull/718
-composer self-update --stable
+#composer self-update --stable
+
+composer self-update
 sed --in-place "s/\"dev-master\":/\"dev-${TRAVIS_COMMIT}\":/" composer.json
