a464487dc14abaf58405a805358b12bfa63c5346
François GERBES <francois.gerbes@libre-informatique.fr>
2017-04-13 15:49:36 +0200

    Add first functional test
    example to use symfony KernelTestCase. it only check if service exist and are initializable. covers nothing in our code...

---
+++ Tests/Functional/BlastCoreBundleTest.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace Blast\CoreBundle\Tests\Functional;
+
+#use PHPUnit\Framework\Assert;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\DependencyInjection\Container;
+
+
+class BlastCoreBundleTest extends KernelTestCase
+{
+
+    protected $container;
+    
+    protected function setUp()
+    {
+        static::bootKernel();
+        
+        /** @var Container $container */
+        $this->container = self::$kernel->getContainer();
+    }
+
+    public function its_services_are_initializable()
+    {
+
+        $serviceIds = array_filter($this->container->getServiceIds(), function ($serviceId) {
+            return 0 === strpos($serviceId, 'blast_core.');
+        });
+
+        foreach ($serviceIds as $serviceId) {
+            echo $serviceId;
+            echo "\n";
+            $this->assertNotNull($this->container->get($serviceId));
+        }
+
+        $this->assertContains('blast_core.form.type.entity_code',$serviceIds);
+    }
+
+
+      public function check_if_services_exists()
+      {
+
+          /*
+           * @Todo is it usefull to test if service exist or not
+          */
+          
+          $this->assertContains('blast_core.code_generator_factory',$this->container->getServiceIds());
+          $this->assertContains('blast_core.code_generators',$this->container->getServiceIds());
+          $this->assertContains('blast_core.form.type.entity_code',$this->container->getServiceIds());
+          $this->assertContains('blast_core.label.strategy.librinfo',$this->container->getServiceIds());
+              
+
+      }
+}
