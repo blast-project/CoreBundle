017ac0b827a2574bfb83e25b5b468998796cfb68
François GERBES <francois.gerbes@libre-informatique.fr>
2017-04-14 20:54:50 +0200

    Indentation and Style

--- Tests/Functional/BlastCoreBundleTest.php
+++ Tests/Functional/BlastCoreBundleTest.php
@@ -1,18 +1,15 @@
 <?php
 
-
 namespace Blast\CoreBundle\Tests\Functional;
 
-#use PHPUnit\Framework\Assert;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\DependencyInjection\Container;
 
-
 class BlastCoreBundleTest extends KernelTestCase
 {
-
+  
     protected $container;
-    
+  
     protected function setUp()
     {
         static::bootKernel();
@@ -23,31 +20,30 @@ class BlastCoreBundleTest extends KernelTestCase
 
     public function testServicesAreInitializable()
     {
-
+        
         $serviceIds = array_filter($this->container->getServiceIds(), function ($serviceId) {
             return 0 === strpos($serviceId, 'blast_core.');
         });
-
+        
         foreach ($serviceIds as $serviceId) {
             $this->assertNotNull($this->container->get($serviceId));
         }
 
-        $this->assertContains('blast_core.form.type.entity_code',$serviceIds);
+        $this->assertContains('blast_core.form.type.entity_code', $serviceIds);
     }
 
 
-      public function testServicesExists()
-      {
+    public function testServicesExists()
+    {
 
-          /*
-           * @Todo is it usefull to test if service exist or not
-          */
+        /*
+         * @todo is it usefull to test if service exist or not
+         */
           
-          $this->assertContains('blast_core.code_generator_factory',$this->container->getServiceIds());
-          $this->assertContains('blast_core.code_generators',$this->container->getServiceIds());
-          $this->assertContains('blast_core.form.type.entity_code',$this->container->getServiceIds());
-          $this->assertContains('blast_core.label.strategy.librinfo',$this->container->getServiceIds());
-              
-
-      }
+        $this->assertContains('blast_core.code_generator_factory', $this->container->getServiceIds());
+        
+        $this->assertContains('blast_core.code_generators', $this->container->getServiceIds());
+        $this->assertContains('blast_core.form.type.entity_code', $this->container->getServiceIds());
+        $this->assertContains('blast_core.label.strategy.librinfo', $this->container->getServiceIds());
+    }
 }
