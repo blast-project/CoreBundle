1e49f4e321ab888d395920a752210e1ac74dbde0
BlastCI <r.et.d@libre-informatique.fr>
2017-06-13 14:12:52 +0200

    DevKit updates (#31)

--- bin/ci-scripts/install_test.sh
+++ bin/ci-scripts/install_test.sh
@@ -3,6 +3,9 @@ set -ev
 
 mkdir --parents "${HOME}/bin"
 
+# Ugly hack
+echo "memory_limit=-1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
+
 composer install --no-interaction --prefer-dist
 composer require --no-interaction --dev phpunit/phpunit 
 composer update --prefer-dist --no-interaction --prefer-stable
@@ -12,11 +15,4 @@ composer update --prefer-dist --no-interaction --prefer-stable
 wget https://github.com/satooshi/php-coveralls/releases/download/v1.0.1/coveralls.phar --output-document="${HOME}/bin/coveralls"
 chmod u+x "${HOME}/bin/coveralls"
 
-# Ugly hack
-echo "memory_limit=-1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
 
-# To be removed when this issue will be resolved: https://github.com/composer/composer/issues/5355
-if [ "${COMPOSER_FLAGS}" = '--prefer-lowest' ]; then
-    composer update --prefer-dist --no-interaction --prefer-stable --quiet
-fi
-composer update --prefer-dist --no-interaction --prefer-stable ${COMPOSER_FLAGS}
