acd5cef4b6d3ca23f853803dfbaab9315e0a3e37
PapsOu <vivien.frasca@libre-informatique.fr>
2017-11-08 12:22:38 +0100

    Stylefix

--- Profiler/Collector.php
+++ Profiler/Collector.php
@@ -1,11 +1,11 @@
 <?php
 
 /*
- * This file is part of the Blast Project package.
+ * This file is part of the Sil Project.
  *
  * Copyright (C) 2015-2017 Libre Informatique
  *
- * This file is licenced under the GNU LGPL v3.
+ * This file is licenced under the GNU GPL v3.
  * For the full copyright and license information, please view the LICENSE.md
  * file that was distributed with this source code.
  */
@@ -79,6 +79,6 @@ class Collector
     {
         $keyStrucure = '#%d %s';
 
-        return sprintf($keyStrucure, count($this->data)+1, $name);
+        return sprintf($keyStrucure, count($this->data) + 1, $name);
     }
 }
