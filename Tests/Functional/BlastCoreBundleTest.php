aa2c9df3a3b5f262b66627ac00b8fe7e70a9703f
François GERBES <francois.gerbes@libre-informatique.fr>
2017-12-18 13:59:22 +0100

    [CI][CoreBundle] Test all service (instantiate)

--- Tests/Functional/BlastCoreBundleTest.php
+++ Tests/Functional/BlastCoreBundleTest.php
@@ -10,44 +10,12 @@
 
 namespace Blast\Bundle\CoreBundle\Tests\Functional;
 
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
-use Symfony\Component\DependencyInjection\Container;
+use Blast\Bundle\TestsBundle\Functional\BlastTestCase;
 
-class BlastCoreBundleTest extends KernelTestCase
+class BlastCoreBundleTest extends BlastTestCase
 {
-    protected $container;
-
-    protected function setUp()
-    {
-        static::bootKernel();
-
-        /* @var Container $container */
-        $this->container = self::$kernel->getContainer();
-    }
-
     public function testServicesAreInitializable()
     {
-        $serviceIds = array_filter($this->container->getServiceIds(), function ($serviceId) {
-            return 0 === strpos($serviceId, 'blast_core.');
-        });
-
-        foreach ($serviceIds as $serviceId) {
-            $this->assertNotNull($this->container->get($serviceId));
-        }
-
-        $this->assertContains('blast_core.form.type.entity_code', $serviceIds);
-    }
-
-    public function testServicesExists()
-    {
-        /*
-         * @todo is it usefull to test if service exist or not
-         */
-
-        $this->assertContains('blast_core.code_generator_factory', $this->container->getServiceIds());
-
-        $this->assertContains('blast_core.code_generators', $this->container->getServiceIds());
-        $this->assertContains('blast_core.form.type.entity_code', $this->container->getServiceIds());
-        //$this->assertContains('blast_core.label.strategy.blast', $this->container->getServiceIds());
+        $this->isServicesAreInitializable('blast');
     }
 }
